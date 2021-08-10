c2c8e8cdab155bf3bd55f8f310a9252046479df4
eigen-spaced <saneyar.k@gmail.com>
2021-08-10 12:27:49 +1000

    Added more file exceptions

--- nvim/.config/nvim/lua/tree-sitter.lua
+++ nvim/.config/nvim/lua/tree-sitter.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.config()
   require'nvim-treesitter.configs'.setup {
     ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ignore_install = { "java", "php", "kotlin", "scala" }, -- List of parsers to ignore installing
+    ignore_install = { "java", "php", "kotlin", "scala", "elixir", "ocaml" }, -- List of parsers to ignore installing
     highlight = {
       enable = true,              -- false will disable the whole extension
       disable = { "cpp", "rust", "go" },  -- list of language that will be disabled
