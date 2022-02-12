af3e7bf11e16a52105845c46bfe94cf1437f85cc
eigen-spaced <saneyar.k@gmail.com>
2022-02-12 19:55:10 +1100

    feat: add symbols outline plugin

---
+++ nvim/.config/nvim/lua/modules/symbols-outline.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+
+function M.setup()
+  local nmap = require("core.utils").nmap
+
+  nmap('<C-s>', '<cmd>SymbolsOutline<CR>')
+end
+
+function M.config()
+  vim.g.symbols_outline = {
+    highlight_hovered_item = true,
+    show_guides = true,
+    width = 35,
+    show_symbol_details = true,
+    preview_bg_highlight = 'Pmenu',
+    keymaps = { -- These keymaps can be a string or a table for multiple keys
+    goto_location = "<Cr>",
+    focus_location = "o",
+    hover_symbol = "<C-space>",
+    toggle_preview = "K",
+    rename_symbol = "r",
+    code_actions = "a",
+  },
+}
+end
+
+return M
