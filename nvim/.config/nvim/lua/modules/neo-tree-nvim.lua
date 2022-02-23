febdc9f42fbb62c1896bb61402ee72a7a58f6331
eigen-spaced <saneyar.k@gmail.com>
2022-02-23 13:42:06 +1100

    refactor(neotree): minor stylistic tweaks

--- nvim/.config/nvim/lua/modules/neo-tree-nvim.lua
+++ nvim/.config/nvim/lua/modules/neo-tree-nvim.lua
@@ -9,13 +9,14 @@ end
 
 function M.config()
   require("neo-tree").setup {
+    close_if_last_window = true,
     follow_current_file = false,
     enable_git_status = true,
     enable_diagnostics = true,
     filesystem = {
       use_libuv_file_watcher = false, -- This will use the OS level file watchers
       window = {
-        width = 30,
+        width = 33,
         mappings = {
           ["o"] = "open",
           ["<c-s>"] = "open_split",
