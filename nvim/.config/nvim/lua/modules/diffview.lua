c20d4d4564e1b083a9ff1837af1d19c31b17a53e
eigen-spaced <saneyar.k@gmail.com>
2022-02-23 15:15:18 +1100

    feat(diffview): add config for diffview.nvim

---
+++ nvim/.config/nvim/lua/modules/diffview.lua
@@ -0,0 +1,32 @@
+local status_ok, diffview = pcall(require, "diffview")
+
+if not status_ok then
+  return
+end
+local cb = require("diffview.config").diffview_callback
+
+diffview.setup {
+  diff_binaries = false,
+  file_panel = {
+    width = 30,
+  },
+  key_bindings = {
+    view = {
+      ["<tab>"] = cb("select_next_entry"),
+      ["<c-tab>"] = cb("select_prev_entry"),
+      ["<leader>ff"] = cb("focus_files"),
+      ["<leader>sf"] = cb("toggle_files"),
+    },
+    file_panel = {
+      ["j"] = cb("next_entry"),
+      ["k"] = cb("prev_entry"),
+      ["<cr>"] = cb("select_entry"),
+      ["o"] = cb("select_entry"),
+      ["r"] = cb("refresh_files"),
+      ["<tab>"] = cb("select_next_entry"),
+      ["<c-tab>"] = cb("select_prev_entry"),
+      ["<leader>ff"] = cb("focus_files"),
+      ["<leader>sf"] = cb("toggle_files"),
+    },
+  },
+}
