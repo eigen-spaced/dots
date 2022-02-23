0e6ce2e948c90bb46a539902e0a68b1f9a444169
eigen-spaced <saneyar.k@gmail.com>
2022-02-23 13:45:10 +1100

    major customizations to feline statusline

--- nvim/.config/nvim/lua/modules/feline-nvim.lua
+++ nvim/.config/nvim/lua/modules/feline-nvim.lua
@@ -4,9 +4,6 @@ if not status_ok then
   return
 end
 
-local lsp = require("feline.providers.lsp")
-local vi_mode_utils = require("feline.providers.vi_mode")
-
 local colors = {
   bg = "#282c34",
   fg = "#abb2bf",
@@ -38,59 +35,25 @@ local vi_mode_colors = {
   NONE = colors.yellow,
 }
 
-local icons = {
-  linux = " ",
-  macos = " ",
-
-  errs = " ",
-  warns = " ",
-  infos = " ",
-  hints = " ",
-
-  lsp = " ",
-  git = "",
-}
-
 local function file_osinfo()
   local os = vim.bo.fileformat:upper()
   local icon
   if os == "UNIX" then
-    icon = icons.linux
+    icon = " "
   elseif os == "MAC" then
-    icon = icons.macos
+    icon = " "
+  else
+    icon = " "
   end
   return icon .. os
 end
 
-local function lsp_diagnostics_info()
-  return {
-    errs = lsp.get_diagnostics_count("Error"),
-    warns = lsp.get_diagnostics_count("Warn"),
-    infos = lsp.get_diagnostics_count("Info"),
-    hints = lsp.get_diagnostics_count("Hint"),
-  }
-end
+local lsp = require("feline.providers.lsp")
+local vi_mode_utils = require("feline.providers.vi_mode")
 
-local function diag_enable(f, s)
-  return function()
-    local diag = f()[s]
-    return diag and diag ~= 0
-  end
-end
-
-local function diag_of(f, s)
-  local icon = icons[s]
-  return function()
-    local diag = f()[s]
-    return icon .. diag
-  end
-end
-
-local function vimode_hl()
-  return {
-    name = vi_mode_utils.get_mode_highlight_name(),
-    fg = vi_mode_utils.get_mode_color(),
-  }
+local lsp_get_diag = function(str)
+  local count = lsp.get_diagnostics_count(str)
+  return (count > 0) and " " .. count .. " " or ""
 end
 
 -- LuaFormatter off
@@ -98,23 +61,40 @@ end
 local comps = {
   vi_mode = {
     left = {
-      provider = "▊",
-      hl = vimode_hl,
-      right_sep = " ",
+      provider = function()
+        return "  " .. vi_mode_utils.get_vim_mode()
+      end,
+      hl = function()
+        local val = {
+          name = vi_mode_utils.get_mode_highlight_name(),
+          fg = vi_mode_utils.get_mode_color(),
+        }
+        return val
+      end,
+      -- right_sep = ' '
     },
     right = {
       provider = "▊",
-      hl = vimode_hl,
+      hl = function()
+        local val = {
+          name = vi_mode_utils.get_mode_highlight_name(),
+          fg = vi_mode_utils.get_mode_color(),
+        }
+        return val
+      end,
       left_sep = " ",
     },
   },
   file = {
     info = {
-      provider = "file_info",
+      -- provider = 'file_info',
+      provider = require("core.file-name").get_current_ufn,
       hl = {
         fg = colors.blue,
         style = "bold",
       },
+      left_sep = " ",
+      right_sep = " ",
     },
     encoding = {
       provider = "file_encoding",
@@ -143,6 +123,18 @@ local comps = {
       style = "bold",
     },
   },
+  position = {
+    provider = "position",
+    left_sep = " ",
+    hl = function()
+      local val = {
+        name = vi_mode_utils.get_mode_highlight_name(),
+        fg = vi_mode_utils.get_mode_color(),
+        style = "bold",
+      }
+      return val
+    end,
+  },
   scroll_bar = {
     provider = "scroll_bar",
     left_sep = " ",
@@ -153,35 +145,51 @@ local comps = {
   },
   diagnos = {
     err = {
-      provider = diag_of(lsp_diagnostics_info, "errs"),
-      left_sep = " ",
-      enabled = diag_enable(lsp_diagnostics_info, "errs"),
+      -- provider = "diagnostic_errors",
+      provider = function()
+        return "✖" .. lsp_get_diag("Error")
+      end,
+      enabled = function()
+        return lsp.diagnostics_exist("Error")
+      end,
       hl = {
         fg = colors.red,
       },
     },
     warn = {
-      provider = diag_of(lsp_diagnostics_info, "warns"),
-      left_sep = " ",
-      enabled = diag_enable(lsp_diagnostics_info, "warns"),
+      -- provider = "diagnostic_warnings",
+      provider = function()
+        return "▲" .. lsp_get_diag("Warn")
+      end,
+      enabled = function()
+        return lsp.diagnostics_exist("Warn")
+      end,
       hl = {
         fg = colors.yellow,
       },
     },
-    info = {
-      provider = diag_of(lsp_diagnostics_info, "infos"),
-      left_sep = " ",
-      enabled = diag_enable(lsp_diagnostics_info, "infos"),
-      hl = {
-        fg = colors.blue,
-      },
-    },
     hint = {
-      provider = diag_of(lsp_diagnostics_info, "hints"),
-      left_sep = " ",
-      enabled = diag_enable(lsp_diagnostics_info, "hints"),
+      -- provider = "diagnostic_hints",
+      provider = function()
+        return "" .. lsp_get_diag("Hint")
+      end,
+      enabled = function()
+        return lsp.diagnostics_exist("Hint")
+      end,
       hl = {
         fg = colors.cyan,
+      },
+    },
+    info = {
+      -- provider = "diagnostic_info",
+      provider = function()
+        return "✱" .. lsp_get_diag("Info")
+      end,
+      enabled = function()
+        return lsp.diagnostics_exist("Info")
+      end,
+      hl = {
+        fg = colors.blue,
       },
     },
   },
@@ -189,7 +197,7 @@ local comps = {
     name = {
       provider = "lsp_client_names",
       left_sep = " ",
-      icon = icons.lsp,
+      icon = " ",
       hl = {
         fg = colors.yellow,
       },
@@ -198,7 +206,7 @@ local comps = {
   git = {
     branch = {
       provider = "git_branch",
-      icon = icons.git,
+      icon = " ",
       left_sep = " ",
       hl = {
         fg = colors.violet,
@@ -226,13 +234,48 @@ local comps = {
   },
 }
 
-local properties = {
+local components = {
+  active = {},
+  inactive = {},
+}
+
+table.insert(components.active, {})
+table.insert(components.active, {})
+table.insert(components.inactive, {})
+table.insert(components.inactive, {})
+
+table.insert(components.active[1], comps.vi_mode.left)
+table.insert(components.active[1], comps.file.info)
+-- table.insert(components.active[1], comps.lsp.name)
+table.insert(components.active[1], comps.diagnos.err)
+table.insert(components.active[1], comps.diagnos.warn)
+table.insert(components.active[1], comps.diagnos.hint)
+table.insert(components.active[1], comps.diagnos.info)
+
+table.insert(components.active[2], comps.git.add)
+table.insert(components.active[2], comps.git.change)
+table.insert(components.active[2], comps.git.remove)
+table.insert(components.active[2], comps.file.os)
+table.insert(components.active[2], comps.git.branch)
+table.insert(components.active[2], comps.scroll_bar)
+table.insert(components.active[2], comps.line_percentage)
+table.insert(components.active[2], comps.position)
+table.insert(components.active[2], comps.vi_mode.right)
+
+table.insert(components.inactive[1], comps.file.info)
+table.insert(components.inactive[1], comps.file.os)
+
+-- LuaFormatter on
+
+feline.setup {
+  colors = { bg = colors.bg, fg = colors.fg },
+  components = components,
+  vi_mode_colors = vi_mode_colors,
   force_inactive = {
     filetypes = {
-      "neotree",
+      "neo-tree",
       "dbui",
       "packer",
-      "startify",
       "fugitive",
       "fugitiveblame",
     },
@@ -240,54 +283,3 @@ local properties = {
     bufnames = {},
   },
 }
-
-local components = {
-  left = {
-    active = {
-      comps.vi_mode.left,
-      comps.file.info,
-      comps.lsp.name,
-      comps.diagnos.err,
-      comps.diagnos.warn,
-      comps.diagnos.hint,
-      comps.diagnos.info,
-    },
-    inactive = {
-      comps.vi_mode.left,
-      comps.file.info,
-    },
-  },
-  mid = {
-    active = {},
-    inactive = {},
-  },
-  right = {
-    active = {
-      comps.git.add,
-      comps.git.change,
-      comps.git.remove,
-      comps.file.os,
-      comps.git.branch,
-      comps.line_percentage,
-      comps.scroll_bar,
-      comps.vi_mode.right,
-    },
-    inactive = {},
-  },
-}
-
--- LuaFormatter on
-
-local M = {}
-
-function M.config()
-  feline.setup {
-    default_bg = colors.bg,
-    default_fg = colors.fg,
-    components = components,
-    properties = properties,
-    vi_mode_colors = vi_mode_colors,
-  }
-end
-
-return M
