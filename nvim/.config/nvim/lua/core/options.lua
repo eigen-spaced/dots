4af619a2981f97ca57b1f0f409d14f6278cbf3b0
eigen-spaced <saneyar.k@gmail.com>
2025-10-15 11:14:34 +0400

    Remove unnecessary config and clean up file

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -4,9 +4,7 @@ local executable = function(e)
   return fn.executable(e) > 0
 end
 
------------------------------------------------------------------------------//
--- Indentation {{{1
------------------------------------------------------------------------------//
+-- Indentation
 set.expandtab = true -- Use spaces instead of tabs
 set.shiftwidth = 2 -- Size of an indent
 set.tabstop = 2 -- Number of spaces tabs count for
@@ -15,9 +13,7 @@ set.smartindent = true -- Insert indents automatically
 set.shiftround = true -- Round indent
 set.joinspaces = false -- No double spaces with join after a dot
 
------------------------------------------------------------------------------//
--- Display {{{1
------------------------------------------------------------------------------//
+-- Display
 set.number = true -- Display line number
 set.relativenumber = true -- Relative line numbers
 set.numberwidth = 2
@@ -54,30 +50,13 @@ set.listchars = {
 }
 -- set.shortmess:append "I" -- disable :intro startup screen
 
------------------------------------------------------------------------------//
--- Title {{{1
------------------------------------------------------------------------------//
+-- Title
 set.titlestring = "❐ %t"
 -- set.titleold = '%{fnamemodify(getcwd(), ":t")}'
 set.title = true
 set.titlelen = 70
 
------------------------------------------------------------------------------//
--- Folds {{{1
------------------------------------------------------------------------------//
--- set.foldexpr = "nvim_treesitter#foldexpr()"
--- set.foldtext =
---   [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) ]]
--- -- set.foldopen:append { "search" }
--- -- set.foldlevelstart = 10
--- set.foldmethod = "expr"
--- -- set.fillchars = "fold:\\"
--- set.foldnestmax = 3
--- set.foldminlines = 1
-
------------------------------------------------------------------------------//
--- Backup {{{1
------------------------------------------------------------------------------//
+-- Backup
 set.swapfile = false
 set.backup = false
 set.writebackup = false
@@ -85,9 +64,7 @@ set.undofile = true -- Save undo history
 set.confirm = true -- prompt to save before destructive actions
 -- set.updatetime = 1000 -- cursor update and swapfile write time. Do not set to 0
 
------------------------------------------------------------------------------//
--- Search {{{1
------------------------------------------------------------------------------//
+-- Search
 set.ignorecase = true -- Ignore case
 set.smartcase = true -- Don't ignore case with capitals
 set.wrapscan = true -- Search wraps at end of file
@@ -95,7 +72,7 @@ set.scrolloff = 5 -- Lines of context
 set.sidescrolloff = 8 -- Columns of context
 set.showmatch = true
 set.inccommand = "nosplit"
-cmd("set nohlsearch")
+-- cmd("set nohlsearch")
 
 -- Use faster grep alternatives if possible
 if executable("rg") then
@@ -106,9 +83,7 @@ end
 
 set.wildignore:append { ".gitignore" }
 
------------------------------------------------------------------------------//
--- window splitting and buffers {{{1
------------------------------------------------------------------------------//
+-- window splitting and buffers
 set.hidden = true -- Enable modified buffers in background
 set.splitbelow = true -- Put new windows below current
 set.splitright = true -- Put new windows right of current
@@ -128,37 +103,8 @@ set.laststatus = 3
 vim.o.sessionoptions =
   "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
 
------------------------------------------------------------------------------//
--- Terminal {{{1
------------------------------------------------------------------------------//
--- Open a terminal pane on the right using :Term
--- cmd [[command Term :botright vsplit term://$SHELL]]
-
--- Terminal visual tweaks
--- Enter insert mode when switching to terminal
--- Close terminal buffer on process exit
-local term_group = api.nvim_create_augroup("Term", { clear = true })
-api.nvim_create_autocmd("TermOpen", {
-  command = "setlocal listchars= nonumber norelativenumber nocursorline",
-  group = term_group,
-})
-api.nvim_create_autocmd("TermOpen", { command = "startinsert" })
-
-cmd([[
-      autocmd BufEnter,BufWinEnter,WinEnter term://* startinsert
-      autocmd BufLeave term://* stopinsert
-      autocmd TermClose term://* call nvim_input('<CR>')
-      autocmd TermClose * call feedkeys("i")
-    ]])
-
------------------------------------------------------------------------------//
--- Mouse {{{1
------------------------------------------------------------------------------//
 set.mouse = "a"
 
------------------------------------------------------------------------------//
--- Filetype {{{1
------------------------------------------------------------------------------//
 vim.g.do_filetype_lua = true
 -- vim.g.did_load_filetypes = false
 
@@ -168,23 +114,13 @@ vim.filetype.add {
   },
 }
 
------------------------------------------------------------------------------//
--- Netrw {{{1
------------------------------------------------------------------------------//
+-- netrw
 -- do not display info on the top of window
 vim.g.netrw_banner = 0
 -- vim.g.netrw_liststyle = 2
 vim.g.netrw_altv = 1
 vim.g.netrw_winsize = 50
 
------------------------------------------------------------------------------//
--- Colorscheme {{{1
------------------------------------------------------------------------------//
 set.termguicolors = true
-
 -- remove those awkward borders from between splits. Looking for a fix in the future
-vim.api.nvim_set_hl(0, "WinSeparator", { bg = "None", fg = "#141414" })
-
------------------------------------------------------------------------------//
--- }}}1
------------------------------------------------------------------------------//
+-- vim.api.nvim_set_hl(0, "WinSeparator", { bg = "None", fg = "#141414" })
