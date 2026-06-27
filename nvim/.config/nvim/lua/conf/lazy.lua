d141c684cb9bf63fcaeef9d2671b9a6b42df2263
eigen-spaced <saneyar.k@gmail.com>
2026-06-27 16:47:47 +0300

    ghostty + nvim: switch theme to Duskfox
    - ghostty: Carbonfox -> Duskfox, font-size 17 -> 18
    - nvim: colorscheme carbonfox -> duskfox (+ lazy install fallback -> duskfox)

--- nvim/.config/nvim/lua/conf/lazy.lua
+++ nvim/.config/nvim/lua/conf/lazy.lua
@@ -17,5 +17,5 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   { import = "plugins" },
 }, {
-  install = { colorscheme = { "terafox", "nightfox" } },
+  install = { colorscheme = { "duskfox", "nightfox" } },
 })
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -11,7 +11,7 @@ return {
           },
         },
       }
-      vim.cmd("colorscheme carbonfox")
+      vim.cmd("colorscheme duskfox")
     end,
   },
 
